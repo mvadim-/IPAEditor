17cd5e8f87ebfa291b11e46edf60d199410bfb59
Vadim Maslov <mvadim@me.com>
2014-11-03 13:02:20 +0200

    Add UI, Add open file

--- IPA editor/AppDelegate.swift
+++ IPA editor/AppDelegate.swift
@@ -20,7 +20,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
 }
 
--- IPA editor/ViewController.swift
+++ IPA editor/ViewController.swift
@@ -9,19 +9,68 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var ipaFileBtn: NSButton!
+    @IBOutlet weak var provisionFileBtn: NSButton!
+    @IBOutlet weak var imageFileBtn: NSButton!
+    @IBOutlet weak var ipaFileLabel: NSTextField!
+    @IBOutlet weak var provisionFileLabel: NSTextField!
+    @IBOutlet weak var imageFileLabel: NSTextField!
+    
+    @IBAction func OpenIpaFile(sender: AnyObject)
+    {
+        self.ipaFileLabel.stringValue = self.openFileWithTypesArray(["ipa"],allowsMultipleSelection: false)[0].lastPathComponent
     }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    @IBAction func openProvisinFile(sender: AnyObject)
+    {
+        self.provisionFileLabel.stringValue = self.openFileWithTypesArray(["mobileprovision"],allowsMultipleSelection: false)[0].lastPathComponent
+    }
+    
+    @IBAction func openImageFile(sender: AnyObject)
+    {
+        let urlArr : Array = self.openFileWithTypesArray(["png"],allowsMultipleSelection: true)
+        var url : String = ""
+        for currUrl in urlArr
+        {
+            url = "\(url.stringByAppendingString(currUrl.lastPathComponent)) "
+        }
+        
+        self.imageFileLabel.stringValue = url
+    }
+    
+    func openFileWithTypesArray(fileTypesArray:Array<String>,allowsMultipleSelection:Bool) -> Array<AnyObject>
+    {
+        var openDlg : NSOpenPanel       = NSOpenPanel()
+        openDlg.canChooseFiles          = true
+        openDlg.allowedFileTypes        = fileTypesArray
+        openDlg.allowsMultipleSelection = allowsMultipleSelection
+        if openDlg.runModal() == NSOKButton
+        {
+            return openDlg.URLs
+        } else if openDlg.runModal() == NSCancelButton {
+            return [""]
+        } else{
+                        let myPopup:NSAlert = NSAlert()
+                        myPopup.messageText = "Error!";
+                        myPopup.informativeText = "Something go wrong."
+                        myPopup.runModal()
+            return [""]
         }
     }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
 }
 
